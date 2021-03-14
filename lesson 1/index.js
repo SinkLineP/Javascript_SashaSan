223b17abef3c0583d5791eba019adcccc76e6dba
SinkLineP <popovartem505@gmail.com>
2021-03-14 15:08:39 +0200

    lesson 3 finish

--- lesson 1/index.js
+++ lesson 1/index.js
@@ -9,5 +9,52 @@ const _name = ''
 const $ = 250
 
 
-console.log(isValid)
-console.log(isNoValid)
+// console.log(isValid)
+// console.log(isNoValid)
+
+
+// 2 Операторы 
+
+// const flash = prompt("Input your name: ")
+// alert("Ваше имя: " + flash);
+
+// console.log("Your name: " + firstName + " age: " + age)
+// console.log(age)
+
+// const y = 5
+// const z = 15
+
+// console.log(y - z)
+// console.log(y + z)
+// console.log(y / z)
+// console.log(y * z)
+
+// let x, y, z, oper
+
+// x = prompt("Type first number")
+// y = prompt("Type second number")
+// oper = prompt("Type an operator")
+
+// if (oper == '+') {
+//     z = Number(x) + Number(y)
+// }
+// if (oper == '-') {
+//     z = Number(x) - Number(y)
+// }
+// if (oper == '*') {
+//     z = Number(x) * Number(y)
+// }
+// if (oper == '/') {
+//     z = Number(x) / Number(y)
+// }
+
+// alert("Your number is: " + z)
+
+x = 6
+y = 3
+
+// console.log(x++)
+// console.log(x)
+
+// console.log(x = x + y)
+console.log(x += y)
