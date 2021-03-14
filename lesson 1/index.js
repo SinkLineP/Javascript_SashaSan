0925a5b165caef110d8dd558a477383bcdc35ef6
SinkLineP <popovartem505@gmail.com>
2021-03-14 13:40:38 +0200

    lesson 2 finish

--- lesson 1/index.js
+++ lesson 1/index.js
@@ -1,3 +1,13 @@
+// 1 Переменные
+
+const firstName = "Artem"
+const lastNAme = 'Popov'
+let age = 17
+const isValid = false
+const isNoValid = true
+const _name = ''
+const $ = 250
 
 
-console.log("Artem")
+console.log(isValid)
+console.log(isNoValid)
